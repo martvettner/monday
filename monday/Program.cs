a8a84da530c86a67556484f3ebe3a99b23aa91f4
mart vettner <martvettner@gmail.com>
2020-11-16 15:46:17 +0200

    Add project files.

---
+++ monday/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace monday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\martv\Desktop\samples";
+            
+            Getdirectories(rootPath);
+            getFiles(rootPath);
+        }
+        public static void Getdirectories(string path)
+        {
+            string[] allDirectories = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+            string filePath = @"C:\Users\martv\Desktop\Monady\directoriesData.txt";
+
+            File.WriteAllLines(filePath, allDirectories);
+
+        }
+        public static void getFiles(string path)
+        {
+            string[] allFiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            string filePath = @"C:\Users\martv\Desktop\Monady\filesData.txt";
+            File.WriteAllLines(filePath, allFiles);
+        }
+    }
+}
